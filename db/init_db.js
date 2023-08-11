6497eefba03db2c5ea81ba72481b656e14c0f884
Noah Mauer <noahmauer1998@gmail.com>
2023-08-11 16:50:46 -0400

    Created favorites, shopping_cart, cart_products, order_history, and reviews tables

--- db/init_db.js
+++ db/init_db.js
@@ -12,12 +12,19 @@ async function buildTables() {
     // drop tables in correct order
 
     await client.query(`
-  DROP TABLE IF EXISTS categories;
-  DROP TABLE IF EXISTS products;
-  DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS reviews;
+    DROP TABLE IF EXISTS order_history;
+    DROP TABLE IF EXISTS cart_products;
+    DROP TABLE IF EXISTS shopping_cart;
+    DROP TABLE IF EXISTS favorites;
+    DROP TABLE IF EXISTS products;
+    DROP TABLE IF EXISTS categories;
+    DROP TABLE IF EXISTS users;
   `)
     console.log("Dropped tables successfully.");
+
     // build tables in correct order
+
     console.log("Starting to build tables...")
     await client.query(`
     CREATE TABLE users(
@@ -29,21 +36,68 @@ async function buildTables() {
       );
     `)
     await client.query(`
+    CREATE TABLE categories(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      description VARCHAR(255)
+      );
+    `)
+    await client.query(`
     CREATE TABLE products(
       id SERIAL PRIMARY KEY,
       title VARCHAR(255) NOT NULL UNIQUE,
       description TEXT NOT NULL,
       price INTEGER NOT NULL,
       stock INTEGER NOT NULL,
-      image VARCHAR(255)
+      image VARCHAR(255),
+      "categoryId" INTEGER REFERENCES categories(id)
       );
     `)
     await client.query(`
-    CREATE TABLE categories(
+    CREATE TABLE favorites(
       id SERIAL PRIMARY KEY,
-      name VARCHAR(255) NOT NULL,
-      description VARCHAR(255)
+      "productId" INTEGER REFERENCES products(id) NOT NULL,
+      "userId" INTEGER REFERENCES users(id) NOT NULL,
+      CONSTRAINT UC_favorites UNIQUE ("productId", "userId")
+    );
+    `)
+    await client.query(`
+    CREATE TABLE shopping_cart(
+      id SERIAL PRIMARY KEY,
+      "userId" INTEGER REFERENCES users(id) NOT NULL UNIQUE,
+      "expirationDate" TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP + interval '1 week'
       );
+    `)
+    await client.query(`
+    CREATE TABLE cart_products(
+      id SERIAL PRIMARY KEY,
+      "shoppingId" INTEGER REFERENCES shopping_cart(id) NOT NULL,
+      "productId" INTEGER REFERENCES products(id) NOT NULL,
+      quatitiy INTEGER NOT NULL,
+      CONSTRAINT UC_cart_products UNIQUE ("shoppingId", "productId")
+    );
+    `)
+    await client.query(`
+    CREATE TABLE order_history(
+      id SERIAL PRIMARY KEY,
+      "productId" INTEGER REFERENCES products(id) NOT NULL,
+      "userId" INTEGER REFERENCES users(id) NOT NULL,
+      quatity INTEGER NOT NULL,
+      completed BOOLEAN DEFAULT false,
+      "dateCreated" TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+      CONSTRAINT UC_order_history UNIQUE ("productId", "userId")
+    )
+    `)
+    await client.query(`
+    CREATE TABLE reviews(
+      id SERIAL PRIMARY KEY,
+      "userId" INTEGER REFERENCES users(id) NOT NULL,
+      "productId" INTEGER REFERENCES products(id) NOT NULL,
+      rating INTEGER NOT NULL,
+      comment TEXT,
+      "isAnonymous" BOOLEAN DEFAULT false,
+      CONSTRAINT UC_reviews UNIQUE ("userId", "productId")
+    );
     `)
     console.log("Created tables successfully.");
 
