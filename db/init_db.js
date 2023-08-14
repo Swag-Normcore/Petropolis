9e32a92a7f8fd8d1f0d1b3fac0aea57c6a0f5b38
Brandon Johnson <developmentbybjohnson@gmail.com>
2023-08-14 09:08:47 -0500

    Created categories db adapters and input some dummy data

--- db/init_db.js
+++ db/init_db.js
@@ -1,8 +1,9 @@
 const {
   client,
+  Category,
   // declare your model imports here
   // for example, User
-} = require('./');
+} = require("./");
 
 async function buildTables() {
   try {
@@ -15,10 +16,10 @@ async function buildTables() {
   DROP TABLE IF EXISTS categories;
   DROP TABLE IF EXISTS products;
   DROP TABLE IF EXISTS users;
-  `)
+  `);
     console.log("Dropped tables successfully.");
     // build tables in correct order
-    console.log("Starting to build tables...")
+    console.log("Starting to build tables...");
     await client.query(`
     CREATE TABLE users(
       id SERIAL PRIMARY KEY,
@@ -27,7 +28,7 @@ async function buildTables() {
       name VARCHAR(255) NOT NULL,
       "isAdmin" BOOLEAN DEFAULT false
       );
-    `)
+    `);
     await client.query(`
     CREATE TABLE products(
       id SERIAL PRIMARY KEY,
@@ -37,16 +38,15 @@ async function buildTables() {
       stock INTEGER NOT NULL,
       image VARCHAR(255)
       );
-    `)
+    `);
     await client.query(`
     CREATE TABLE categories(
       id SERIAL PRIMARY KEY,
       name VARCHAR(255) NOT NULL,
       description VARCHAR(255)
       );
-    `)
+    `);
     console.log("Created tables successfully.");
-
   } catch (error) {
     throw error;
   }
@@ -54,10 +54,27 @@ async function buildTables() {
 
 async function populateInitialData() {
   try {
+    console.log("Creating Categories...");
+
+    const categoryToCreate = await Category.createCategory({
+      name: "T-Shirts",
+      description: "XL Mens Taylor Swift Shirt",
+    });
+    //   {
+    //     name: "Bags",
+    //     description: "Black womens toot bag",
+    //   },
+    // ];
+
+    // console.log("Category Created:");
+    console.log(categoryToCreate);
+
+    console.log("Finshed creating categories.");
     // create useful starting data by leveraging your
     // Model.method() adapters to seed your db, for example:
     // const user1 = await User.createUser({ ...user info goes here... })
   } catch (error) {
+    console.error("Error creating categories");
     throw error;
   }
 }
