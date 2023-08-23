ed55849d1e3670888c466aa144acec0e9cb1a6a8
NoahM98 <noahmauer1998@gmail.com>
2023-08-23 16:37:19 -0400

    Installed jotai and setup initial atoms

---
+++ src/atoms.js
@@ -0,0 +1,23 @@
+import { atom } from "jotai";
+
+export const counterAtom = atom(0);
+
+export const tokenAtom = atom("asdf");
+
+export const adminAtom = atom(true);
+
+export const userAtom = atom({});
+
+export const productsAtom = atom([]);
+
+export const categoriesAtom = atom([]);
+
+export const favoritesAtom = atom([]);
+
+export const ordersAtom = atom([]);
+
+export const shoppingCartAtom = atom([]);
+
+export const canvasAtom = atom(false);
+
+export const apiHealthAtom = atom("");
